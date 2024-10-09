3d748f5107753d6e06f6ba7e7dbbeb8048802e59
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-10-09 14:18:49 -0300

    feat:Making GenerateTokenAction and AuthController

---
+++ app/Actions/GenerateTokenAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Actions;
+use Illuminate\Foundation\Auth\User;
+
+class GenerateTokenAction
+{
+    public function __construct(
+        private readonly User $user,
+    )
+    {}
+
+    public function execute()
+    {
+        return $this->generateToken();
+    }
+
+    private function generateToken():string
+    {
+        return $this->user->type == "Candidato"
+            ? $this->user->createToken('token', ['candidate-token'])->plainTextToken
+            : $this->user->createToken('token', ['recruiter-token'])->plainTextToken;
+    }
+}
--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Actions\GenerateTokenAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthRequest;
-use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function authUser(AuthRequest $authRequest)
+    public function authUser(AuthRequest $authRequest):JsonResponse
     {
         $credentials = $authRequest->validated();
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Email ou senha incorretos.',
+            ], 400);
+        }
+
+        $user = Auth::user();
+        $token = new GenerateTokenAction($user)->execute();
+        return response()->json([
+            'message' => 'Authorized',
+            'token'=> $token,
+        ], 200);
     }
 }
