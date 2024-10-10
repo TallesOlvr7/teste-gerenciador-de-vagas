2e6185d4cbdd8f1d0b574bd257e0a45a85d16511
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-10-10 01:51:04 -0300

    feat:Make CreateUserRequest

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'require|min:6',
+            'type'=>'required'
+        ];
+    }
+
+    public function messages():array
+    {
+        return [
+            'name.required'=>'Nome não inserido.',
+            'email.required'=>'Email não iniserido.',
+            'email.email'=>'Insira um email válido.',
+            'email.unique'=>'Email já registrado',
+            'password.require'=>'Senha não inserida.',
+            'password.min'=>'A senha deve ter no mínimo seis dígitos.',
+            'type.required'=>'Selecione o tipo de conta.'
+        ];
+    }
+}
