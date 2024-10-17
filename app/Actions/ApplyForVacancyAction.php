04e0bd0edae2200f76319b52140a4f4050da0632
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-10-17 20:17:08 -0300

    refactor:Remaking ActionLogic and Validations

--- app/Actions/ApplyForVacancyAction.php
+++ app/Actions/ApplyForVacancyAction.php
@@ -19,19 +19,31 @@ class ApplyForVacancyAction
 
     private function applyForVacancy():UserResource
     {
-        $this->verifyVacancyStatus();
+        $vacancy = $this->getVacancyForApply();
 
         $this->user
         ->vacancies()
-        ->attach($this->vacancyId);
+        ->attach($vacancy->id);
         
-        return new UserResource($this->user->vacancies);
+        return new UserResource($this->user->load('vacancies'));
     }
 
-    private function verifyVacancyStatus():void
+    private function getVacancyForApply():object
     {
         $vacancy = DB::table('vacancies')->where('id','=',$this->vacancyId)
-        ->get();
+        ->first();
+        $this->verifyApplication($vacancy);
+
+        return $vacancy;
+    }
+
+    private function verifyApplication(object $vacancy):void
+    {
+        $alreadyApplied = $this->user->vacancies()->where('vacancy_id', $vacancy->id)->exists();
+
+        if ($alreadyApplied) {
+            throw new \Exception("Você já está inscrito nessa vaga.");
+        }
 
         if($vacancy->status != 'Aberta'){
             throw new \Exception("A vaga está fechada para inscrições.");
