e26c62f4583509bc182dc19aeeb6d23f8e8ba15a
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-10-18 03:56:44 -0300

    feat:Making Vacancy method index and GetVacanciesAction

---
+++ app/Actions/GetVacanciesAction.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Actions;
+
+use App\Http\Resources\VacancyCollection;
+use DB;
+use Illuminate\Database\Query\Builder;
+
+class GetVacanciesAction
+{
+    private Builder $query;
+    public function __construct(
+        private readonly string|null $param,
+        private readonly string|null $status,
+        private readonly string|null $type,
+    ) {
+        $this->query = DB::table('vacancies');
+    }
+
+    public function execute(): VacancyCollection
+    {
+        return $this->getVacancies();
+    }
+
+    private function getVacancies(): VacancyCollection
+    {
+        if ($this->param || $this->status || $this->type) {
+            $this->query = $this->makeQuery($this->query);
+            return new VacancyCollection(
+                $this->query->paginate(20)
+            );
+        }
+
+        return new VacancyCollection($this->query->where('status', '=', 'Aberta')->paginate(20));
+    }
+
+    private function makeQuery(Builder $query): Builder
+    {
+        if ($this->param && $this->status && $this->type) {
+            $query = $this->withStatusFilter($query);
+            $query = $this->withTypeFilter($query);
+            $query = $this->withParamFilter($query);
+            return $query;
+        }
+
+        if ($this->param && $this->status) {
+            $query = $this->withStatusFilter($query);
+            $query = $this->withParamFilter($query);
+            return $query;
+        }
+
+        if ($this->param && $this->type) {
+            $query = $this->withTypeFilter($query);
+            $query = $this->withParamFilter($query);
+            return $query;
+        }
+
+        if ($this->status && $this->type) {
+            $query = $this->withStatusFilter($query);
+            $query = $this->withTypeFilter($query);
+            return $query;
+        }
+
+        if ($this->param) {
+            $query = $this->withParamFilter($query);
+            return $query;
+        }
+
+        if ($this->type) {
+            $query = $this->withTypeFilter($query);
+            return $query;
+        }
+
+        if ($this->status) {
+            $query = $this->withStatusFilter($query);
+            return $query;
+        }
+
+        return $query;
+    }
+
+    private function withParamFilter(Builder $query): Builder
+    {
+        return $query->whereAny([
+            'title',
+            'description'
+        ], '=', "%{$this->param}%");
+    }
+
+    private function withTypeFilter(Builder $query): Builder
+    {
+        return $query->where('type', '=', $this->type);
+    }
+
+    private function withStatusFilter(Builder $query): Builder
+    {
+        return $query->where('type', '=', $this->type);
+    }
+}
--- app/Http/Controllers/API/V1/VacancyController.php
+++ app/Http/Controllers/API/V1/VacancyController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Actions\GetVacanciesAction;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class VacancyController extends Controller
 {
 
-    public function index()
+    public function index(Request $request):JsonResponse
     {
-        //
+        $status = $request->input('status');
+        $type = $request->input('type');
+        $param = $request->input('search');
+        $vacancies = (new GetVacanciesAction($param,$status,$type))->execute();
+        return response()->json($vacancies,200);
     }
 
 
