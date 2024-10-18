0549067bdbf9a3a017ffac9f7c65713a4cc4d8b3
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-10-18 05:06:29 -0300

    refactor:Remaking routes

--- routes/api.php
+++ routes/api.php
@@ -12,16 +12,16 @@ Route::prefix('v1')->group(function(){
     Route::middleware('auth:sanctum')->group(function(){
         Route::post('/logout',[AuthController::class,'logout']);
         Route::apiResource('users',UserController::class)->except(['index','store']);
+        Route::apiResource('vacancies',VacancyController::class)->only(['index','show']);
     });
 
     Route::middleware(['auth:sanctum','abilities:candidate-token'])->group(function(){ 
-        Route::apiResource('vacancies',VacancyController::class)->only(['index','show']);
         Route::post('/apply/{user}',[UserController::class,'apply']);
     });
 
     Route::middleware(['auth:sanctum','abilities:recruiter-token'])->group(function(){
         Route::get('/users',[UserController::class,'index']);
-        Route::apiResource('vacancies',VacancyController::class);
+        Route::apiResource('vacancies',VacancyController::class)->except(['index','show']);
         Route::patch('/cancel', [VacancyController::class, 'cancel']);
     });
 });
