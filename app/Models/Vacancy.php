5e520fac3aa8f8a669dc0a3b4c07de169cf85e71
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-11-05 15:39:58 -0300

    refactor:Making relationship between user and vacancy model

--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Vacancy extends Model
@@ -21,4 +22,10 @@ class Vacancy extends Model
     {
         return $this->belongsToMany(User::class);
     }
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
